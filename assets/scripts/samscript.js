4bbd681f0cebec1a9d0c0665e9c5b1f4436852fa
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-06-04 12:44:59 -0400

    cleansed the data based on new user input form

--- assets/scripts/samscript.js
+++ assets/scripts/samscript.js
@@ -2,55 +2,36 @@ function init() {
     // staticly generated data for dev purpose
     const genData = [
         {
-            "name": "Nerea Whitley",
-            "phone": "1-717-230-2234",
-            "email": "egestas.a.dui@aol.com",
-            "address": "552-3118 Eget Rd.",
-            "postalZip": "75932",
-            "numberrange": 17,
-            "time": "7:45 AM",
-            "date": "2024.07.02"
-        },
-        {
-            "name": "Owen Ayala",
-            "phone": "1-268-733-7127",
-            "email": "elit@yahoo.ca",
-            "address": "P.O. Box 408, 1309 Sapien Avenue",
-            "postalZip": "73563",
-            "numberrange": 336,
-            "time": "1:35 AM",
-            "date": "2023.03.02"
-        },
-        {
-            "name": "Linus Brooks",
-            "phone": "1-352-898-8015",
-            "email": "elementum.dui.quis@icloud.edu",
-            "address": "102-3165 Et Street",
-            "postalZip": "858631",
-            "numberrange": 366,
-            "time": "6:54 AM",
-            "date": "2024.06.02"
-        },
-        {
-            "name": "Jamalia Puckett",
-            "phone": "1-676-955-3206",
-            "email": "cras.eget@yahoo.com",
-            "address": "Ap #641-9098 Commodo St.",
-            "postalZip": "7234-6679",
-            "numberrange": 79,
-            "time": "10:25 AM",
-            "date": "2023.04.24"
-        },
-        {
-            "name": "Veda Boyer",
-            "phone": "1-602-244-7471",
-            "email": "eu.dolor@aol.edu",
-            "address": "319-7228 Mauris St.",
-            "postalZip": "WO7V 7YN",
-            "numberrange": 317,
-            "time": "9:39 PM",
-            "date": "2024.08.20"
-        }
+            "id": 1,
+            "full-name": "Dannye Colbron",
+            "email": "dcolbron0@eventbrite.com",
+            "phone": "259-926-8025",
+            "address": "12719 Blackbird Crossing",
+            "city": "Little Current",
+            "prov": "Ontario",
+            "postalZip": "J6A",
+            "date": "2023-07-20"
+          }, {
+            "id": 2,
+            "full-name": "Web Domerc",
+            "email": "wdomerc1@paypal.com",
+            "phone": "378-731-1527",
+            "address": "45252 Troy Point",
+            "city": "Mississauga",
+            "prov": "Ontario",
+            "postalZip": "L5W",
+            "date": "2023-10-28"
+          }, {
+            "id": 3,
+            "full-name": "Maurine Teggart",
+            "email": "mteggart2@craigslist.org",
+            "phone": "364-334-9182",
+            "address": "2472 Anniversary Plaza",
+            "city": "North York",
+            "prov": "Ontario",
+            "postalZip": "M3H",
+            "date": "2023-06-12"
+          }
     ];
     var apptInfo = [];
     $.each(genData, function (){
@@ -77,7 +58,7 @@ function init() {
     function saveToLocalStorage(apptObj){
         console.log(apptObj);
         console.log(apptObj.date);
-        console.log(apptObj.time.splice(" "));
+        console.log(apptObj.time);
         
         // var myDateSearchKey = `${apptObj.date}-${apptObj.time.splice(" ")}`;
         // console.log(myDateSearchKey);
