9c6d0723951e8d34f29a0630e9751829c14af539
Rajni bala <balatamna16@gmail.com>
2023-06-05 22:59:39 -0400

    created function weatherforecast

--- scriptrbala.js
+++ scriptrbala.js
@@ -28,3 +28,41 @@ confirmAppointment.keypress(function (event) {
     canadianholidays(date);
   }
 });
+
+// function weatherForecast(searchTerm) 
+function weatherForecast(searchCity) {
+
+  fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + searchCity + "&appid=b4ed94adc674507878b0aeb7f93e988b&units=imperial")
+    .then(function (response) {
+      // making sure we have status 200 ok from server before parsing data
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log("this is the weather", data);
+          $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
+
+          //loop to create a 5 days weather forecast`
+          for (var i = 0; i < data.list.length; i++) {
+
+            if (data.list[i].dt_txt.indexOf("15:00:00") !== -1) {
+
+              var titleFivedays = $("<h3>").text(new Date(data.list[i].dt_txt).toLocaleDateString());
+              var imgFivedays = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+              var colFivedays = $("<div>");
+              var cardFivedays = $("<div>");
+              var cardBodyFivedays = $("<div>");
+              var windFivedays = $("<p>").text("Wind: " + data.list[i].wind.speed + " MPH");
+              var humidFivedays = $("<p>").text("Humidity: " + data.list[i].main.humidity + "%");
+              var tempFivedays = $("<p>").text("Temperature: " + data.list[i].main.temp + " Â°F");
+
+
+              //combine together and display on page
+              colFivedays.append(cardFivedays.append(cardBodyFivedays.append(titleFivedays, imgFivedays, tempFivedays, humidFivedays, windFivedays)));
+              //append card to column, body to card, and other elements to body
+              $("#forecast .row").append(colFivedays);
+            }
+          };
+        });
+      };
+
+    });
+}
