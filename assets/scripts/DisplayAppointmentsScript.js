70b2918dbd7cb9e22feafa221fd096b68c3215e2
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-06-07 12:42:53 -0400

    weather icon works

--- assets/scripts/DisplayAppointmentsScript.js
+++ assets/scripts/DisplayAppointmentsScript.js
@@ -138,7 +138,13 @@ function init() {
     function renderAppointments() {
         displayAreaRight.empty();
         for (i = 0; i < apptInfo.length; i++) {
-            displayAreaRight.append(`<article class="tile is-child notification is-info"><p class="subtitle">${apptInfo[i].fullName} on ${apptInfo[i].date}, at ${apptInfo[i].time}</p><p>${apptInfo[i].address}, ${apptInfo[i].city}, ${apptInfo[i].prov} | Phone: ${apptInfo[i].phone}<span>    icons</span></p></article>`);
+            // get weather
+            // var weather = getWeather(apptInfo[i].city, apptInfo[i].date);
+            var weather = "https://openweathermap.org/img/wn/10d@2x.png"
+            // get Holiday
+            // var statHoliday = getHoliday(apptInfo[i].date);
+            
+            displayAreaRight.append(`<article class="tile is-child notification is-info"><p class="subtitle">${apptInfo[i].fullName} on ${apptInfo[i].date}, at ${apptInfo[i].time}</p><p>${apptInfo[i].address}, ${apptInfo[i].city}, ${apptInfo[i].prov} | Phone: ${apptInfo[i].phone}<span></span><span><figure class="image is-48x48"><img src=${weather} alt="Placeholder image"></figure></span></p></article>`);
         }
     };
     function saveToLocalStorage(apptObj) {
